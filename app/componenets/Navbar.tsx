4b70704e41c735d9a266e790866dace2c375df1b
elghanbibadr <bghanbi50@gmail.com>
2025-04-21 13:17:49 +0100

    fix prod issue

--- app/componenets/Navbar.tsx
+++ app/componenets/Navbar.tsx
@@ -2,7 +2,6 @@
 import Image from "next/image";
 import Link from "next/link";
 import logo from "@/public/solar-match-logo.svg";
-import { ArrowRight } from "lucide-react";
 
 const Navbar = () => {
 
