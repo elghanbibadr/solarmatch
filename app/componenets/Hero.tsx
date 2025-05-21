b808dd330c1b1a3f65ebad7eb8fd300aaf1e0762
elghanbibadr <bghanbi50@gmail.com>
2025-05-21 19:23:53 +0100

    remove margin top from the hero

--- app/componenets/Hero.tsx
+++ app/componenets/Hero.tsx
@@ -8,7 +8,7 @@ export default function Hero() {
   return (
     <div
       id="hero"
-      className="md:grid md:grid-cols-2 my-10 gap-10 px-4 md:px-6 max-w-[1300px] mx-auto justify-center justify-items-center place-items-center "
+      className="md:grid md:grid-cols-2  gap-10 px-4 md:px-6 max-w-[1300px] mx-auto justify-center justify-items-center place-items-center "
     >
       <div data-aos="fade-right" className="md:my-12   max-w-[1200px] mx-auto ">
         <div className="  ">
